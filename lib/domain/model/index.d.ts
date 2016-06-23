7ee41bdd550a28551c9f8e878873667ea153a913
Stephan Schuler <stephan.schuler@netlogix.de>
2016-06-23 22:10:58 +0200

    Delete local lib folder

--- lib/domain/model/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { ResourceProxy } from './resource-proxy';
-export { Type } from './type';
-export { Payload } from './payload';
-export { Property, PropertyInterface } from './property';
-export { Uri } from './uri';
--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './service';
-export * from './domain/model';
-export * from './domain/repository';
